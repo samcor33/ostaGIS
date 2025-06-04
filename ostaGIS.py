8ade762bdb6fe8c68923b8cc85138f5bb5f2a3ef
samcor33 <samcor33@gmail.com>
2025-06-04 06:33:46 -0700

    Update ostaGIS.py

--- ostaGIS.py
+++ ostaGIS.py
@@ -349,3 +349,71 @@ def wfb_country_scrape(saved_html, output_folder, log="CRITICAL"):
     wfb_coded.to_csv(output_folder + "\\wfb_countries_coded.csv")
 
     return(print(f"finished...see {output_folder}"))
+
+
+# ******* WFB TOS GEOSCRAPER ********************************************************************
+# ******* WFB TOS GEOSCRAPER ********************************************************************
+# ******* WFB TOS GEOSCRAPER ********************************************************************
+def wfb_tos_geoscraper(saved_html):
+    """
+    saved_html: path to the saved CIA wfb html file -- see documentation 'https://github.com/samcor33/ostaGIS/'
+    
+    Description: scrape CIA desingated bad actors for known area(s) of operation (AORs)
+    Result: a Pandas Dataframe to be used for further processing
+    ___________________________________
+    |    index    | area of operation |
+    |---------------------------------|
+    | torg (name) |  AOR description  |
+    
+    Designed for: use with the `parse_country` function to geolocate the AORs
+    """
+    from bs4 import BeautifulSoup, NavigableString
+    import pandas as pd
+    import country_converter as coco
+    
+    wfb_tos = {}
+    
+    # Open the HTML file
+    with open(saved_html, 'r', encoding='utf-8') as file:
+        html_content = file.read()
+    
+    # Parse the HTML content
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
+    # Find div elements labeled "pb30" which hold the information for each t_org
+    sections = soup.find_all('div', attrs={"class":"pb30"})
+    
+    # Extract information (example: all paragraph tags)
+    for s in sections:    
+        titles = s.find_all("h2") # Title(name) of t_org
+        paragraphs = s.find_all('p') # p elements with t_org information
+    
+        # Get each h2 element
+        for title in titles:
+    
+            # Get information in each p element
+            for p in paragraphs:        
+                
+                # Find elements that contain "areas of operation" and split to extract the text to "wfb_tos" dict
+                if 'areas of operation' in p.text:
+                    cat_l = "area of operation"
+                    cat_t = p.text.split('areas of operation – ')[1]
+                
+                    if title.text not in wfb_tos:
+                        wfb_tos[title.text] = {}
+                    wfb_tos[title.text][cat_l] = cat_t
+                    break
+    
+                # Find elements that contain "area(s) of operation" and split to extract the text to "wfb_tos" dict
+                # This is due to variation in labeling from the CIA website
+                if 'area(s) of operation' in p.text:
+                    cat_l = "area of operation"
+                    cat_t = p.text.split('area(s) of operation – ')[1]
+                
+                    if title.text not in wfb_tos:
+                        wfb_tos[title.text] = {}
+                    wfb_tos[title.text][cat_l] = cat_t
+                    break
+    
+    # Put "wfb_tos" into pandas dataframe
+    return pd.DataFrame(wfb_tos).T.reset_index()
